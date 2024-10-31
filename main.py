82a0daca64a0c33c301ea292cab93590c3e7929a
Daniel Lee <daniel8lee58@gmail.com>
2024-10-31 15:53:02 -0500

    edits

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ def validate_post_id(post_id: str) -> bool:
     return (post_id + ".md") in file_names
 
 def add_post(post_id: str):
+    post_title = input("Title of post: ")
+
     markdown_file_path = f"{local_dir}/markdown/{post_id}.md"
 
     # Manually ensure that the min-width is 50% of the page
@@ -25,8 +27,28 @@ def add_post(post_id: str):
     subprocess.run(
         f"cd {local_dir} && pandoc -o posts/{post_id}.html -s --highlight-style my.theme {markdown_file_path}", 
         stdout=subprocess.DEVNULL,
+        stderr=subprocess.STDOUT,
         shell=True,
     )
+
+    # Need to add link to post in index.html
+    index_file_path = f"{local_dir}/index.html"
+    with open(index_file_path, "r") as file:
+        contents = file.read()
+        lines = contents.splitlines(keepends=True)
+        link_html = f"<a href='posts/{post_id}.html'>{post_title}</a>"
+        if link_html not in contents:
+            target_line_num = -1
+            for i, line in enumerate(lines):
+                if "<div>Daniel's Blog</div>" in line:
+                    target_line_num = i + 1
+            if target_line_num == -1:
+                raise Exception("Did not find </body> in index.html")
+            lines.insert(target_line_num, "\t" + link_html + "\n")
+
+    with open(index_file_path, "w") as file:
+        file.writelines(lines)
+
 
     with open(markdown_file_path, "w") as file:
         file.write(original_text)
@@ -44,26 +66,11 @@ def handle_add():
     post_id = get_valid_post_id("Id of the post you would like to add:\n")
     add_post(post_id)
 
-def handle_delete():
-    post_id = get_valid_post_id("Id of the post you would like to delete:\n")
-    add_post(post_id)
-
-def handle_refresh():
-    post_id = get_valid_post_id("Id of the post you would like to refresh (input 'all' to refresh all posts):\n")
-    add_post(post_id)
-
-
 def run():
-    command = input("What would you like to do? Options: add, delete, refresh\n")
+    command = input("What would you like to do? Options: add\n")
     match command.strip():
         case "add":
             handle_add()
-        case "delete":
-            handle_delete()
-            pass
-        case "refresh":
-            handle_refresh()
-            pass
         case _:
             print("Invalid input")
             run()
