f1f4f7f4115028b631e2191bff725e1eec7bc0c4
Daniel Lee <daniel8lee58@gmail.com>
2024-10-31 18:37:44 -0500

    edits

---
+++ instructions.md
@@ -0,0 +1,7 @@
+To add a new blog post:
+1. Create a new markdown file in `/markdown`
+2. Write the blog post in that file
+3. Add the id and title of the post to `posts-info.json`. 
+   The id of the post is the name of the markdown file, excluding the file extension.
+   The title of the post is whatever you want it to be.
+4. Run `python3 refresh.py`.
--- refresh.py
+++ refresh.py
@@ -1,5 +1,6 @@
 import subprocess
 import os
+import json
 
 local_dir = os.path.dirname(os.path.realpath(__file__))
 
@@ -8,9 +9,7 @@ def validate_post_id(post_id: str) -> bool:
     file_names = [s.strip() for s in result.stdout.split("\n")]
     return (post_id + ".md") in file_names
 
-def add_post(post_id: str):
-    post_title = input("Title of post: ")
-
+def add_post(post_id: str, post_title: str):
     markdown_file_path = f"{local_dir}/markdown/{post_id}.md"
 
     # Manually ensure that the min-width is 50% of the page
@@ -25,9 +24,10 @@ def add_post(post_id: str):
         file.write(new_text)
 
     subprocess.run(
-        f"cd {local_dir} && pandoc -o posts/{post_id}.html -s --highlight-style my.theme {markdown_file_path}", 
+        f"pandoc -o posts/{post_id}.html -s --highlight-style my.theme {markdown_file_path}", 
         stdout=subprocess.DEVNULL,
         stderr=subprocess.STDOUT,
+        cwd=local_dir,
         shell=True,
     )
 
@@ -40,39 +40,34 @@ def add_post(post_id: str):
         if link_html not in contents:
             target_line_num = -1
             for i, line in enumerate(lines):
-                if "<div>Daniel's Blog</div>" in line:
+                if "LINKS TO POSTS WILL GET GENERATED BELOW THIS LINE" in line:
                     target_line_num = i + 1
+                    break
             if target_line_num == -1:
-                raise Exception("Did not find </body> in index.html")
+                raise Exception("Did not find placeholder comment in index.html")
             lines.insert(target_line_num, "\t" + link_html + "\n")
 
     with open(index_file_path, "w") as file:
         file.writelines(lines)
 
-
     with open(markdown_file_path, "w") as file:
         file.write(original_text)
 
-def get_valid_post_id(prompt: str) -> str:  
-    post_id = input(prompt)
-    valid = validate_post_id(post_id)
-    if not valid:
-        print("Invalid post id")
-        return get_valid_post_id(prompt)
-    else: 
-        return post_id
-
-def handle_add():
-    post_id = get_valid_post_id("Id of the post you would like to add:\n")
-    add_post(post_id)
-
 def run():
-    command = input("What would you like to do? Options: add\n")
-    match command.strip():
-        case "add":
-            handle_add()
-        case _:
-            print("Invalid input")
-            run()
+    with open(f"{local_dir}/posts-info.json") as file:
+        # Delete index.html
+        subprocess.run("rm index.html && cp index-template.html index.html", cwd=local_dir, shell=True)
+
+        posts_info_json = json.load(file)
+        posts = posts_info_json["posts"]
+        posts = reversed(posts) # Reverse so that posts appear in the correct order after being added
+        for post in posts:
+            id = post["id"]
+            title = post["title"]
+            if not validate_post_id(id):
+                print(f"{id} is not a valid post id")
+                return
+            add_post(id, title)
 
 run()
+
